cb8218119193e3455df7656ef93c9b2919331f98
Oscavian <dev@oscavian.com>
2023-10-19 16:18:33 +0200

    add mounting script for post install mediums

---
+++ mount-install-mediums.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VM_BASE_FOLDER=`VBoxManage list systemproperties | sed -n -e 's/Default machine folder: *\(\/.*\)$/\1/p'`
+WS_ISO_PATH=/run/media/oskar/2e1063cc-b9dc-4ac0-ad88-26721ac14aa7/isos/en-us_windows_server_2022_x64_dvd_620d7eac.iso
+SQL_ISO_PATH=/run/media/oskar/2e1063cc-b9dc-4ac0-ad88-26721ac14aa7/isos/en_sql_server_2019_standard_x64_dvd_814b57aa.iso
+SP_IMAGE_PATH=/run/media/oskar/2e1063cc-b9dc-4ac0-ad88-26721ac14aa7/isos/officeserver.img
+
+function mount_install_med {    
+    VBoxManage storageattach "$1" \
+            --storagectl SATA \
+            --port 1 \
+            --type dvddrive \
+            --medium "$2"
+}
+
+mount_install_med "sqlserver" $SQL_ISO_PATH
+mount_install_med "sharepoint" $SP_IMAGE_PATH
