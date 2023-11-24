96ca08eeb6020e1990a8d8be23089d6ba2fc1492
Oscavian <dev@oscavian.com>
2023-11-24 03:36:19 +0100

    Add SSMS install script

---
+++ scripts/domcon/ssms.ps1
@@ -0,0 +1,11 @@
+
+$media_path = "$env:HOMEDRIVE\$env:HOMEPATH\SSMS-Setup-ENU.exe"
+if((Test-Path $media_path) -eq $false) {
+    Write-Progress "Downloading SSMS Setup file..."
+    Invoke-WebRequest -Uri 'https://aka.ms/ssmsfullsetup' -OutFile $env:HOMEDRIVE\$env:HOMEPATH\SSMS-Setup-ENU.exe
+}
+
+$install_path = "$env:SystemDrive\SSMSto"
+$params = "/Install SSMSInstallRoot=`"$install_path`"" # /Quiet
+
+Start-Process -FilePath $media_path -ArgumentList $params -Wait
